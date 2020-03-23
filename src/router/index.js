7ae49ef523a6d6fb70f164231900afbf11c9e8c9
is-nan <38523283+is-nan@users.noreply.github.com>
2020-03-23 13:50:37 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -12,29 +12,27 @@ import React,{lazy} from 'react';
 const Home = lazy(() => import('../views/Blog/Home'))
 const Article = lazy(() => import('../views/Blog/Article'))
 const Login = lazy(() => import('../views/Blog/Login'))
-const Admin = lazy(() => import('../views/admin/Layouts'))
+const Layouts = lazy(() => import('../views/admin/Layouts'))
 const AdminArticle = lazy(() => import('../views/admin/Article'))
 const AdminComment = lazy(() => import('../views/admin/Comment'))
 const AdminLink = lazy(() => import('../views/admin/Link'))
 const Error = lazy(() => import('../components/Error'))
 const About = lazy(() => import('../views/Blog/About'))
 //博客页面路由
-const BlogRouter = [
-    {path: '/', component: Home},
+export const Blog = {path: '/', component: Home}
+export const BlogRouter = [
     {path: '/Article', component: Article},
     {path: '/Login', component: Login},
     {path: '/About',component: About },
     {path: '*', component: Error}
 ]
 //后台页面路由
-const AdminRouter={
-    path: '/admin', component: Admin,
-    children: [
-        {path: '/admin/Article', component: AdminArticle},
-        {path: '/admin/Comment', component: AdminComment},
-        {path: '/admin/Link', component: AdminLink},
-        {path: '*', component:Error}
-    ]
+export const Admin={
+    path: '/admin',component:Layouts,
 }
-
-export default {BlogRouter,AdminRouter}
+export const AdminRouter=[
+    {path: '/admin/Article', component: AdminArticle},
+    {path: '/admin/Comment', component: AdminComment},
+    {path: '/admin/Link', component: AdminLink},
+    {path: '*', component:Error}
+]
